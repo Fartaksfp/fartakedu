1838f0e4438115b4d12c53b904c32de20e62dd3f
Fartak Dev <support@fartaksfp.com>
2025-10-19 15:42:47 +0330

    add : hero images transition effect

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,8 @@
 import HeroText from "./HeroText";
 import HeroImage from "./HeroImage";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Search } from "lucide-react";
 
 function Hero() {
   return (
@@ -9,8 +12,17 @@ function Hero() {
       {/* overlay */}
       <div className="absolute inset-0 bg-black/50"></div>
 
-      <div className="relative h-[100dvh] flex items-center justify-center">
+      <div className="relative h-[100dvh] flex flex-col items-center justify-center">
         <HeroText />
+        <div className="relative">
+          <Input
+            className="w-80 mt-5 bg-white text-black p-6"
+            placeholder="دنبال چه دوره ای هستی؟"
+          />
+          <Button className="absolute top-7 left-2">
+            <Search />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- components/home/HeroImage.tsx
+++ components/home/HeroImage.tsx
@@ -1,14 +1,42 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const heroSlides = [
+  "/images/heroSlides/slide1.jpg",
+  "/images/heroSlides/slide2.jpg",
+  "/images/heroSlides/slide3.jpg",
+  "/images/heroSlides/slide4.jpg",
+  "/images/heroSlides/slide5.jpg",
+  "/images/heroSlides/slide6.jpg",
+];
 
 function HeroImage() {
+  const [imageIndex, setimageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setimageIndex((prev) => (prev + 1) % heroSlides.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-      <Image
-        src="/images/ai-course.jpg"
-        alt="AI Course Photo"
-        fill
-        className="object-cover blur-xs"
-      />
-  )
+    <>
+      {heroSlides.map((src, index) => (
+        <Image
+          key={index}
+          src={src}
+          alt={`Hero Slide ${index + 1}`}
+          fill
+          className={`object-cover transition-opacity duration-1000 ${
+            index === imageIndex ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      ))}
+    </>
+  );
 }
 
-export default HeroImage
+export default HeroImage;
