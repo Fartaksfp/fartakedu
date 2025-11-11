fbfdea5d4612682068b10b225ddf9adb6f36f5e1
Fartak Dev <support@fartaksfp.com>
2025-11-11 12:51:38 +0330

    add: loading file /courses

---
+++ app/(main)/courses/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='h-dvh justify-center items-center'>
+      <p>در حال بارگزاری...</p>
+    </div>
+  )
+}
+
+export default Loading
