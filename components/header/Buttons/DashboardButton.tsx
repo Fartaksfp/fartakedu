76cce176bb3232714559ea9794b87bc95eae9745
Fartak Dev <support@fartaksfp.com>
2025-11-15 15:08:55 +0330

    add: button for move back to main site

--- components/header/Buttons/DashboardButton.tsx
+++ components/header/Buttons/DashboardButton.tsx
@@ -8,7 +8,7 @@ function DashboardButton() {
     <Link href={'/dashboard'}>
     <Button
       variant={"outline"}
-      className="flex flex-row-reverse mx-10 items-center gap-1 text-sm md:text-base"
+      className="flex flex-row-reverse items-center gap-1 text-sm md:text-base"
     >
       <ChevronLeft className="w-4 h-4 md:w-5 md:h-5" />
       <LayoutDashboard className="w-4 h-4 md:w-5 md:h-5" />
--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -2,10 +2,12 @@ import Logo from "../shared/Logo";
 import Navbar from "./Navbar";
 import Themetoggle from "../shared/Themetoggle/Themetoggle";
 import LoginButtonRender from "./LoginButtonRender";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 function MainHeader() {
   return (
-    <header className="p-4 md:px-7 z-10 bg-white dark:bg-black fixed w-full flex justify-between items-center border-b border-border transition-all duration-300">
+    <header className="p-4 md:px-1\  z-10 bg-white dark:bg-black fixed w-full flex justify-between items-center border-b border-border transition-all duration-300">
       <div className="pr-6 md:pr-20">
         <Logo width="160px" />
       </div>
@@ -18,6 +20,11 @@ function MainHeader() {
       </div>
 
       <div className="sm:flex flex-row-reverse items-center gap-3 md:gap-5 pl-4 md:pl-20 hidden">
+        <Link href={'https://fartaksfp.com'}>
+          <Button>
+            بازگشت به سایت
+          </Button>
+        </Link>
         <LoginButtonRender />
         <Themetoggle />
       </div>
