720bb2e15bb74df92c80f57b36732082b0399b6b
Fartak Dev <support@fartaksfp.com>
2025-11-26 16:12:42 +0330

    fix this

--- app/api/receipt/route.ts
+++ app/api/receipt/route.ts
@@ -2,9 +2,10 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-    console.log('request : ' , request);
-    const body = await request.json();
-    console.log('body : ' , body);
-    
-    redirect("/dashboard/receipt?status=success");
+  console.log("request : ", request);
+  const text = await request.text();
+  const body = Object.fromEntries(new URLSearchParams(text));
+  console.log("body : ", body);
+
+  redirect("/dashboard/receipt?status=success");
 }
