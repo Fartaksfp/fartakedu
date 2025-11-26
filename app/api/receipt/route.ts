136fde3533611a4c4e17712a52271405226dc5d5
Fartak Dev <support@fartaksfp.com>
2025-11-26 16:00:07 +0330

    try to log request

--- app/api/receipt/route.ts
+++ app/api/receipt/route.ts
@@ -2,8 +2,9 @@ import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
+    console.log('request : ' , request);
     const body = await request.json();
-
-    console.log(body);
+    console.log('body : ' , body);
+    
     redirect("/dashboard/receipt?status=success");
 }
