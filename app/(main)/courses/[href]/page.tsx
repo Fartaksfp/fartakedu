6b4c63834580cb9df5c869f8cdc83a0776d31d84
Fartak Dev <support@fartaksfp.com>
2025-12-14 08:55:34 +0330

    style: footer and courses page

--- app/(main)/courses/[href]/page.tsx
+++ app/(main)/courses/[href]/page.tsx
@@ -7,6 +7,31 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import { toShamsi } from "@/helper/toShamsi";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ href: string }>;
+}) {
+  const { href } = await params;
+  const data = await getCourse(href);
+  if (data?.status >= 400) {
+    return {
+      title: "خطا در دریافت اطلاعات | فرتاک",
+    };
+  }
+  const course = await data?.coursesdata;
+
+  if (data?.status === 404) {
+    return {
+      title: "دوره یافت نشد | فرتاک",
+    };
+  } else {
+    return {
+      title: `${course.title} | فرتاک`,
+    };
+  }
+}
+
 async function page({ params }: { params: Promise<{ href: string }> }) {
   const { href } = await params;
 
@@ -18,7 +43,7 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
     const course = await data?.coursesdata;
     const courseDate = toShamsi(course.created_at);
     return (
-      <div className="container mx-auto px-4 sm:px-6 py-10">
+      <div className="container mx-auto mt-10 px-4 sm:px-20 py-10">
         <Suspense
           fallback={
             <div className="flex items-center justify-center min-h-[60vh]">
@@ -38,7 +63,7 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
             </div>
 
             <div className="lg:col-span-2 flex flex-col justify-between space-y-4">
-              <h1 className="text-2xl sm:text-3xl font-bold">{course.title}</h1>
+              <h1 className="text-2xl leading-12 sm:text-3xl font-bold">{course.title}</h1>
               <p className="text-sm sm:text-base text-muted-foreground leading-relaxed">
                 {course.description}
               </p>
@@ -105,7 +130,9 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
                   <p className="font-medium text-lg sm:text-xl">
                     {course.price.toLocaleString()} تومان
                   </p>
-                  {course.status === "soon" && <p>تخفیف ویژه مخصوص شرکت ها در سبد خرید اعمال میگردد</p>}
+                  {course.status === "soon" && (
+                    <p>تخفیف ویژه مخصوص شرکت ها در سبد خرید اعمال میگردد</p>
+                  )}
                 </div>
               </div>
               <div className="pt-4">
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,10 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <MainHeader />
+      <main className="flex-grow">
       {children}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
