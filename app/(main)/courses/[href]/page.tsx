21f0308407d0c3a4404f5b98195a09ee26878d0d
Fartak Dev <support@fartaksfp.com>
2025-12-14 11:05:57 +0330

    update courses photo and styles

--- app/(main)/courses/[href]/page.tsx
+++ app/(main)/courses/[href]/page.tsx
@@ -7,6 +7,31 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import { toShamsi } from "@/helper/toShamsi";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ href: string }>;
+}) {
+  const { href } = await params;
+  const data = await getCourse(href);
+  if (data?.status >= 400) {
+    return {
+      title: "خطا در دریافت اطلاعات | فرتاک",
+    };
+  }
+  const course = await data?.coursesdata;
+
+  if (data?.status === 404) {
+    return {
+      title: "دوره یافت نشد | فرتاک",
+    };
+  } else {
+    return {
+      title: `${course.title} | فرتاک`,
+    };
+  }
+}
+
 async function page({ params }: { params: Promise<{ href: string }> }) {
   const { href } = await params;
 
@@ -18,7 +43,7 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
     const course = await data?.coursesdata;
     const courseDate = toShamsi(course.created_at);
     return (
-      <div className="container mx-auto px-4 sm:px-6 py-10">
+      <div className="container mx-auto mt-10 px-4 sm:px-20 py-10">
         <Suspense
           fallback={
             <div className="flex items-center justify-center min-h-[60vh]">
@@ -27,7 +52,7 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
           }
         >
           <div className="grid mt-10 grid-cols-1 border-2 dark:border-gray-500 gap-6 rounded-xl shadow-lg p-4 lg:grid-cols-3 lg:gap-8 lg:p-6">
-            <div className="lg:col-span-1 h-90 sm:h-110 relative w-full rounded-xl overflow-hidden">
+            <div className="lg:col-span-1 relative w-full rounded-xl overflow-hidden aspect-square">
               <Image
                 src={course.image}
                 alt={course.title + " تصویر"}
@@ -38,7 +63,7 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
             </div>
 
             <div className="lg:col-span-2 flex flex-col justify-between space-y-4">
-              <h1 className="text-2xl sm:text-3xl font-bold">{course.title}</h1>
+              <h1 className="text-2xl leading-12 sm:text-3xl font-bold">{course.title}</h1>
               <p className="text-sm sm:text-base text-muted-foreground leading-relaxed">
                 {course.description}
               </p>
@@ -105,7 +130,9 @@ async function page({ params }: { params: Promise<{ href: string }> }) {
                   <p className="font-medium text-lg sm:text-xl">
                     {course.price.toLocaleString()} تومان
                   </p>
-                  {course.status === "soon" && <p>تخفیف ویژه مخصوص شرکت ها در سبد خرید اعمال میگردد</p>}
+                  {course.status === "soon" && (
+                    <p>تخفیف ویژه مخصوص شرکت ها در سبد خرید اعمال میگردد</p>
+                  )}
                 </div>
               </div>
               <div className="pt-4">
